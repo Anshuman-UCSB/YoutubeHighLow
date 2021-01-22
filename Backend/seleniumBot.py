96eae3b4318f2ac977ce460ce9adc167aeef621c
Anshuman-UCSB <anshuman@ucsb.edu>
2021-01-22 14:16:04 -0800

    working

--- Backend/seleniumBot.py
+++ Backend/seleniumBot.py
@@ -1,5 +1,6 @@
 import selenium
 import re
+from selenium.webdriver.common.by import By
 from selenium.webdriver import Chrome
 
 browser = Chrome('./webdrivers/windows/chromedriver.exe')
@@ -7,8 +8,9 @@ browser = Chrome('./webdrivers/windows/chromedriver.exe')
 def scrape(url, count = 10):
     browser.get(url)
     raw = browser.page_source
-    print(re.findall(r"href=\"/watch\?v=\w+\"", raw)) #find all of type [href="/watch?v=]
-    
+    out = re.findall(r"href=\"(/watch\?v=\w+)\"", raw)[:count] #find all of type [href="/watch?v=]
+    return list(map(lambda x: "youtube.com"+x, out))
+
 def paramToUrl(query):
     base = "https://www.youtube.com/results?search_query="
     out = ""
@@ -19,7 +21,18 @@ def paramToUrl(query):
             out+=c
     return base+out
 
+def getThumbnailUrl(url):
+    browser.get("http://www.get-youtube-thumbnail.com/")
+    elem = browser.find_element_by_id("youtubeLink")
+    elem.send_keys(url)
+    button = browser.find_element_by_id("get")
+    button.click()
+    imgUrl = browser.find_element_by_id("copyimageURL")
+    return imgUrl.get_attribute("value")
 
 if __name__ == '__main__':
-    print(paramToUrl("I want to goog42l1e this!"))
-    # scrape("https://www.youtube.com/results?search_query=drama")
+    query = input("Enter a search query:\n > ")
+    urls = scrape(paramToUrl(query))
+    print(urls)
+    
+    print(getThumbnailUrl("youtube.com/watch?v=bzp3vt5aGDo"))
